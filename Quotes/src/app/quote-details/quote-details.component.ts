1e14b900d2814e0f351fc67857b18a3b4724b0c5
kamransumar <kamransumar@gmail.com>
2018-08-10 15:04:12 +0300

    added quote details

---
+++ Quotes/src/app/quote-details/quote-details.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {Quote} from '../quote';
+
+
+@Component({
+  selector: 'app-quote-details',
+  templateUrl: './quote-details.component.html',
+  styleUrls: ['./quote-details.component.css']
+})
+export class QuoteDetailsComponent implements OnInit {
+
+@Input() quote: Quote;
+@Output() isDelete = new EventEmitter<boolean>();
+  constructor() { }
+  quoteDelete(complete: boolean) {
+    this.isDelete.emit(complete);
+  }
+
+  votes: number;
+
+  constructor() {
+    this.votes = 0;
+
+  }
+
+ voteUp(): boolean {
+  this.votes += 1;
+  return false;
+}
+
+  voteDown(): boolean {
+    this.votes -= 1;
+    return false;
+  }
+
+
+  ngOnInit() {
+  }
+
+}
---
+++ Quotes/src/app/quote-page.directive.spec.ts
@@ -0,0 +1,8 @@
+import { QuotePageDirective } from './quote-page.directive';
+
+describe('QuotePageDirective', () => {
+  it('should create an instance', () => {
+    const directive = new QuotePageDirective();
+    expect(directive).toBeTruthy();
+  });
+});
